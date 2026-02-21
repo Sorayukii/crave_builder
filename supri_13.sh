496e6d103f39f4c06f7cadc9f47d788818ac0ff8
Sorayukii <sorayukii69@gmail.com>
2026-02-21 18:53:18 +0700

    Update supri_13.sh
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- supri_13.sh
+++ supri_13.sh
@@ -9,7 +9,7 @@ rm -rf vendor/sony
 rm -rf vendor/lineage-priv
 
 # Initialize repo
-repo init -u https://github.com/k4ngcaribug/manifest.git -b QPR3
+repo init -u https://github.com/Superior13-NEXT/manifest.git -b QPR3
 
 # Sync the repositories
 /opt/crave/resync.sh
@@ -22,7 +22,7 @@ rm -rf packages/apps/Settings
 rm -rf frameworks/base
 
 # Clone device tree
-git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b stock kernel/sony/sdm845
+git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b stock-upstream kernel/sony/sdm845
 git clone https://github.com/Sorayukii/android_device_sony_aurora -b supri-13 device/sony/aurora
 git clone https://github.com/Sorayukii/android_device_sony_tama-common -b aosp-13 device/sony/tama-common
 git clone https://github.com/Sorayukii/android_hardware_sony_SonyOpenTelephony -b 13 hardware/sony/SonyOpenTelephony
@@ -31,9 +31,10 @@ git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 13
 git clone https://github.com/Sorayukii/priv-keys -b master vendor/lineage-priv
 
 # Clone Additional
-git clone https://github.com/KanonifyX/android_packages_apps_Settings -b QPR3 packages/apps/Settings
-git clone https://github.com/KanonifyX/android_packages_apps_SuperiorLab -b QPR3 packages/apps/SuperiorLab
-git clone https://github.com/KanonifyX/android_frameworks_base -b QPR3  frameworks/base
+git clone https://github.com/LineageOS/android_packages_apps_Aperture --depth=1 -b lineage-20.0 packages/apps/Aperture
+git clone https://github.com/KanonifyX/android_packages_apps_Settings --depth=1 -b QPR3 packages/apps/Settings
+git clone https://github.com/KanonifyX/android_packages_apps_SuperiorLab --depth=1 -b QPR3 packages/apps/SuperiorLab
+git clone https://github.com/KanonifyX/android_frameworks_base --depth=1 -b QPR3  frameworks/base
 
 # Export
 export BUILD_USERNAME=ivy
