1114d8366e7a97ec9e8fd1d7099b390ade31ce9e
Sorayukii <sorayukii69@gmail.com>
2026-03-28 09:06:42 +0700

    Update test.sh
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- test.sh
+++ test.sh
@@ -12,8 +12,8 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-23.2 --git-lfs 
 repo sync
 
 # Clone device tree
-git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b stock kernel/sony/sdm845
-git clone https://github.com/Sorayukii/android_device_sony_aurora -b 15 device/sony/aurora
+git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b main kernel/sony/sdm845
+git clone https://github.com/Sorayukii/android_device_sony_aurora -b 16x device/sony/aurora
 git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 15 device/sony/tama-common
 git clone https://github.com/Sorayukii/android_hardware_sony_SonyOpenTelephony -b 15 hardware/sony/SonyOpenTelephony
 git clone https://github.com/Sorayukii/proprietary_vendor_sony_aurora -b 15 vendor/sony/aurora
