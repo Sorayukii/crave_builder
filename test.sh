805e315269304f2c8cec3c1fc1c432e2a23fed0e
Sorayukii <sorayukii69@gmail.com>
2026-03-29 13:24:00 +0700

    Fix branch names in device tree cloning commands
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- test.sh
+++ test.sh
@@ -14,8 +14,8 @@ repo sync
 
 # Clone device tree
 git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b main kernel/sony/sdm845
-git clone https://github.com/Sorayukii/android_device_sony_aurora -b 16x device/sony/aurora
-git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 15 device/sony/tama-common
+git clone https://github.com/Sorayukii/android_device_sony_aurora -b 15 device/sony/aurora
+git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 16x device/sony/tama-common
 git clone https://github.com/Sorayukii/android_hardware_sony_SonyOpenTelephony -b 15 hardware/sony/SonyOpenTelephony
 git clone https://github.com/Sorayukii/proprietary_vendor_sony_aurora -b 15 vendor/sony/aurora
 git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 15 vendor/sony/tama-common
