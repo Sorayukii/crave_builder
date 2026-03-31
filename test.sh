5c04dd1deb8bdcf0c6fdc92e4c4676829d701338
Sorayukii <sorayukii69@gmail.com>
2026-04-01 05:29:39 +0700

    Update test.sh
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- test.sh
+++ test.sh
@@ -37,7 +37,7 @@ chmod +x ./fuck-bpf/apply.sh && ./fuck-bpf/apply.sh --mb
 
 # Patch
 wget https://raw.githubusercontent.com/ximi-daisy-test/daisy_manifests/refs/heads/master/lineageos-23.2/0014-fw_b-hwui-return_to_ashmem.patch
-patch -p1 < 0014-fw_b-hwui-return_to_ashmem.patch
+patch -p1 <0014-fw_b-hwui-return_to_ashmem.patch
 
 # Export
 export BUILD_USERNAME=ivy
