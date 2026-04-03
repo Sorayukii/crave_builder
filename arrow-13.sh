d8cbc317c811d91039f16eb581daa0193aec2588
Sorayukii <sorayukii69@gmail.com>
2026-04-04 05:24:48 +0700

    Update and rename arrow_13.sh to arrow-13.sh
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- arrow-13.sh
+++ arrow-13.sh
@@ -24,6 +24,9 @@ git clone https://github.com/Sorayukii/proprietary_vendor_sony_akari -b 13 vendo
 git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 13 vendor/sony/tama-common
 # git clone https://github.com/Sorayukii/priv-keys -b master vendor/lineage-priv
 
+# Clone libncurses
+git clone https://github.com/LineageOS/android_external_libncurses -b lineage-20.0 external/libncurses
+
 # Export
 export BUILD_USERNAME=ivy
 export BUILD_HOSTNAME=crave
