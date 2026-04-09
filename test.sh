f095d0970bfcb9f7b3fc591168aa1863e366cfee
Sorayukii <sorayukii69@gmail.com>
2026-04-09 20:53:11 +0700

    Update test.sh
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- test.sh
+++ test.sh
@@ -19,16 +19,12 @@ repo sync -c --force-sync --no-clone-bundle --no-tags
 
 # Clone device tree
 git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b stock kernel/sony/sdm845
-git clone https://github.com/Sorayukii/android_device_sony_aurora -b evix-15 device/sony/aurora
+git clone https://github.com/Sorayukii/android_device_sony_aurora -b evox-15 device/sony/aurora
 git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 15x device/sony/tama-common
 git clone https://github.com/Sorayukii/android_hardware_sony_SonyOpenTelephony -b 15 hardware/sony/SonyOpenTelephony
 git clone https://github.com/Sorayukii/proprietary_vendor_sony_aurora -b 15 vendor/sony/aurora
 git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 15 vendor/sony/tama-common
 git clone https://github.com/Sorayukii/priv-keys -b master vendor/lineage-priv
-
-# Fuck-bpf
-git clone https://github.com/techyminati/fuck-bpf -b lineage-23.2
-chmod +x ./fuck-bpf/apply.sh && ./fuck-bpf/apply.sh --mb
 
 # Export
 export BUILD_USERNAME=ivy
