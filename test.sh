33460e420be0ebaeae9d51880fbf4bd452c1e148
Sorayukii <sorayukii69@gmail.com>
2026-04-09 17:31:55 +0700

    Update test.sh
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 
 # Remove local changes
 rm -rf .repo/local_manifests
-rm -rf prebuilts
 rm -rf kernel/sony/sdm845
 rm -rf device/sony/tama-common
 rm -rf device/sony/aurora
@@ -12,19 +11,19 @@ rm -rf vendor/sony/aurora
 rm -rf vendor/lineage-priv
 
 # Initialize repo
-repo init -u https://github.com/AviumUI/android_manifests -b avium-16.2 --git-lfs
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 
 # Sync the repositories
 /opt/crave/resync.sh
 repo sync -c --force-sync --no-clone-bundle --no-tags
 
 # Clone device tree
-git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b personal kernel/sony/sdm845
-git clone https://github.com/Sorayukii/android_device_sony_aurora -b avm device/sony/aurora
-git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 16x device/sony/tama-common
+git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b stock kernel/sony/sdm845
+git clone https://github.com/Sorayukii/android_device_sony_aurora -b evix-15 device/sony/aurora
+git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 15x device/sony/tama-common
 git clone https://github.com/Sorayukii/android_hardware_sony_SonyOpenTelephony -b 15 hardware/sony/SonyOpenTelephony
 git clone https://github.com/Sorayukii/proprietary_vendor_sony_aurora -b 15 vendor/sony/aurora
-git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 16x vendor/sony/tama-common
+git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 15 vendor/sony/tama-common
 git clone https://github.com/Sorayukii/priv-keys -b master vendor/lineage-priv
 
 # Fuck-bpf
@@ -37,5 +36,5 @@ export BUILD_HOSTNAME=crave
 
 # Build rom
 . build/envsetup.sh
-lunch lineage_aurora-bp4a-userdebug
-m bacon -j$(nproc --all)
+lunch lineage_aurora-bp1a-userdebug
+m evolution
