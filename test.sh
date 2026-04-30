4add7bf2785457fc6fda3032232799773e0a51fb
Sorayukii <sorayukii69@gmail.com>
2026-05-01 04:21:58 +0700

    Update branch name for tama-common device clone
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- test.sh
+++ test.sh
@@ -96,7 +96,7 @@ start_build_process() {
     # Clone device tree
     git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b bpf kernel/sony/sdm845
     git clone https://github.com/Sorayukii/android_device_sony_aurora -b axion-16 device/sony/aurora
-    git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 16x device/sony/tama-common
+    git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 16ax device/sony/tama-common
     git clone https://github.com/Sorayukii/android_hardware_sony_SonyOpenTelephony -b 15 hardware/sony/SonyOpenTelephony
     git clone https://github.com/Sorayukii/proprietary_vendor_sony_aurora -b 15 vendor/sony/aurora
     git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 16x vendor/sony/tama-common
