cbacbcabd0d44ba4a8712ec48f0c7beec130e273
Sorayukii <sorayukii69@gmail.com>
2026-05-02 05:09:26 +0700

    Update branch versions for device tree clones
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- test.sh
+++ test.sh
@@ -98,8 +98,8 @@ start_build_process() {
 
     # Clone device tree
     git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b bpf kernel/sony/sdm845
-    git clone https://github.com/Sorayukii/android_device_sony_aurora -b 15 device/sony/aurora
-    git clone https://github.com/Sorayukii/android_device_sony_tama-common -b luna-16.2 device/sony/tama-common
+    git clone https://github.com/Sorayukii/android_device_sony_aurora -b 16.2-luna device/sony/aurora
+    git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 16 device/sony/tama-common
     git clone https://github.com/Sorayukii/android_hardware_sony_SonyOpenTelephony -b 15 hardware/sony/SonyOpenTelephony
     git clone https://github.com/Sorayukii/proprietary_vendor_sony_aurora -b 15 vendor/sony/aurora
     git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 15 vendor/sony/tama-common
