90a110bcd3be50f7f5aea5cfc69cd1d49149c0e0
Sorayukii <sorayukii69@gmail.com>
2026-05-01 17:57:42 +0700

    Update test.sh
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- test.sh
+++ test.sh
@@ -98,11 +98,11 @@ start_build_process() {
 
     # Clone device tree
     git clone https://github.com/Sorayukii/stardust_kernel_sony_sdm845 -b bpf kernel/sony/sdm845
-    git clone https://github.com/Sorayukii/android_device_sony_aurora -b axion-16 device/sony/aurora
-    git clone https://github.com/Sorayukii/android_device_sony_tama-common -b 16ax device/sony/tama-common
+    git clone https://github.com/Sorayukii/android_device_sony_aurora -b 15 device/sony/aurora
+    git clone https://github.com/Sorayukii/android_device_sony_tama-common -b cr-16.0 device/sony/tama-common
     git clone https://github.com/Sorayukii/android_hardware_sony_SonyOpenTelephony -b 15 hardware/sony/SonyOpenTelephony
     git clone https://github.com/Sorayukii/proprietary_vendor_sony_aurora -b 15 vendor/sony/aurora
-    git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 16x vendor/sony/tama-common
+    git clone https://github.com/Sorayukii/proprietary_vendor_sony_tama-common -b 15 vendor/sony/tama-common
     git clone https://github.com/Sorayukii/priv-keys -b master vendor/lineage-priv
 
     # Fix camera
